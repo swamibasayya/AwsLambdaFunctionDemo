a9b4a57a85485475ff2bbb8f9fe5d8d60a4218ad
swamibasayya <swamibasayya161985@gmail.com>
2025-08-02 17:04:52 +1000

    added
    added

--- src/MyLambdaFunctionDemo/Function.cs
+++ src/MyLambdaFunctionDemo/Function.cs
@@ -1,4 +1,7 @@
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Net.NetworkInformation;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +10,52 @@ namespace MyLambdaFunctionDemo;
 
 public class Function
 {
-    
+
+    private readonly IAmazonS3 _s3Client;
+    public Function()
+    {
+        _s3Client=new AmazonS3Client();
+
+    }
+
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async  Task<string> FunctionHandler(object input,  ILambdaContext context)
     {
-        return input.ToUpper();
+        context.Logger.LogLine("FunctionHandler invoked");
+
+
+        var bucketName = "rosterdata2025";
+        var key = $"Crew-Data-{DateTime.UtcNow:yyyy-MM-dd-HH-mm-ss}.txt";
+        var content = "Sample content from .NET Lambda";
+
+        var putRequest = new PutObjectRequest
+        {
+            BucketName = bucketName,
+            Key = key,
+            ContentBody = content,
+            ContentType = "text/plain"
+        };
+        context.Logger.LogLine($"File name{key}");
+
+        try
+        {
+            var response = await _s3Client.PutObjectAsync(putRequest);
+            return $"Uploaded to s3://{bucketName}/{key}";
+        }
+        catch (Exception ex)
+        {
+            return $"Error: {ex.Message}";
+        }
+
+
+        
+        
+         
     }
 }
